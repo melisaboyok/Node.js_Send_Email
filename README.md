18d3dd5b7617c4093b999684a6b85240fd916540
Name Surname <melisa.boyok101@gmail.com>
2023-04-18 23:26:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node.js
+Sending Email with Nodemailer Package
---
+++ app.js
@@ -0,0 +1,21 @@
+var nodemailer = require("nodemailer");
+
+var transfor = nodemailer.createTransport({
+    service: "gmail", // gönderen mailin kullandığı servis
+    auth:{ // gönderecek kişinin mail bilgileri
+        user:"user@gmail.com",
+        pass:"123456"
+    }
+});
+
+var mailBilgi = {
+    from:"user@gmail.com",
+    to:"receivers@gmail.com", // gönderilen kişinin mail bilgileri
+    subject: "Node js ile mail", // Başlık 
+    text: "Bu bir test mesajıdır"
+};
+
+transfor.sendMail(mailBilgi, function(error){
+    if(error) console.log(error);
+    else console.log("Mailiniz gönderildi");
+});
